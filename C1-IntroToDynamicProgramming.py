f171b4f84ccddde9e37cfbdca6f50a96803bbc89
isaakKareithi <isaacsgk2@gmail.com>
2026-01-05 15:56:14 +0300

    C1-IntroToDynamicProgramming.py

---
+++ C1-IntroToDynamicProgramming.py
@@ -0,0 +1,36 @@
+# dynamic programming python implementation
+#  of min cost path problem
+R = 3
+C = 3
+
+def minCost(cost, m, n):
+
+    # instead of following line, we can use int tc[m+1][n+1]
+    # or dynamically allocate memory to save space. The following
+    # line is used to keep the program simple and
+    # maike it working on all compilers
+
+    tc = [[0 for x in range(C)] for x in range(R)]
+
+    tc[0][0] = cost[0][0]
+
+    # initialixe first column of total cost(tc) array
+    for i in range(1, m+1):
+        tc[i][0] = tc[i-1][0] + cost[i][0]
+
+    # initialize first row of tc array 
+    for j in range(1, n+1):
+        tc[0][j] = tc[0][j-1] + cost[0][j]
+
+    # construct rest of the tc array
+    for i in range(1, m+1): 
+        for j in range(1, n+1):
+            tc[i][j] = min(tc[i-1][j-1], tc[i-1][j], tc[i][j-1])
+
+    return tc[m][n]
+
+# Driver code 
+cost = [[1, 2, 3],
+        [4, 8, 2],
+        [1, 5, 2]]
+print(minCost(cost, 2, 2))
